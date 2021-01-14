58dbbe790386e734380e92feb51cee01096d6ef9
arthurye <soyellowjack@gmail.com>
2021-01-14 10:24:43 +0800

    add a new question 1018 可被 5 整除的二进制前缀 位运算 + 数学分析

--- leetcodetest/src/part_1/section1.java
+++ leetcodetest/src/part_1/section1.java
@@ -42,19 +42,23 @@ public class section1 {
 //        pairs.add(pair2);
 //        System.out.println(smallestStringWithSwaps(s, pairs));
 
-        // 684
-        /**
-         * [[1,2], [1,3], [2,3]] 无向图边连接关系
-         *       1
-         *     *   *
-         *    2  *  3
-         */
-        int[][] edges = {{1, 2}, {1, 3}, {2, 3}};
-        List<Integer> result_list = new ArrayList<Integer>();
-        for(int item : findRedundantConnection(edges)) {
-            result_list.add(item);
-        }
-        System.out.println(result_list);
+//        // 684
+//        /**
+//         * [[1,2], [1,3], [2,3]] 无向图边连接关系
+//         *       1
+//         *     *   *
+//         *    2  *  3
+//         */
+//        int[][] edges = {{1, 2}, {1, 3}, {2, 3}};
+//        List<Integer> result_list = new ArrayList<Integer>();
+//        for(int item : findRedundantConnection(edges)) {
+//            result_list.add(item);
+//        }
+//        System.out.println(result_list);
+
+        // 1018
+        int[] A = {1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1};
+        System.out.println(prefixesDivBy5(A));
 
     }
 
@@ -285,6 +289,34 @@ public class section1 {
 
     public static void union(int[] parent, int index1, int index2) {
         parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    // 1018 可被 5 整除的二进制前缀 位运算 + 数学分析
+    public static List<Boolean> prefixesDivBy5(int[] A) {
+        /**
+         * 思路，能被5整除的十进制数，任意一个数减去个位数的值后都是一个整数，整数都能被5整除
+         * 所以本题十位数以上一定能被5整除不用考虑，只用考虑到每次得到的十进制数的个位能否被5
+         * 整除即可
+         **/
+        int size = A.length;
+        int sum = 0;
+        List<Boolean> result_list = new ArrayList<Boolean>();
+        // // 1.可读性高 方便理解版
+        // for(int i = 0; i < size; ++i) {
+        //     sum <<= 1; // 每次上一个数做位运算左移一位变成下一个数的十位数
+        //     sum += A[i]; // 下一个数等于上一个十进制数加上当前最后一位二进制
+        //                 // (最后一位二进制数无论是0或者1都和换算成十进制数值都一样所以可以直接加)
+        //     sum %= 10; // 取个位数
+        //     result_list.add(sum % 5 == 0);
+        // }
+
+        // 2.减少代码冗余版 根据数论中同余运算规则 sum %= 10 等价于 sum %= 5
+        for(int item : A) {
+            result_list.add( ( sum = ((sum << 1) + item) % 5 ) == 0);
+        }
+
+        return result_list;
+
     }
 
 }
