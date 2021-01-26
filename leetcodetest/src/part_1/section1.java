6215accf3e2446bdd06528d8cfcff0e59764f18b
arthurye <soyellowjack@gmail.com>
2021-01-26 11:23:01 +0800

    add one question 1128. 等价多米诺骨牌对的数量 二元数组，正反相等

--- leetcodetest/src/part_1/section1.java
+++ leetcodetest/src/part_1/section1.java
@@ -77,9 +77,13 @@ public class section1 {
 //        }
 //        System.out.println(array2list);
 
-        // 628
-        int[] nums = {1, 2, 3, 4};
-        System.out.println(maximumProduct(nums));
+//        // 628
+//        int[] nums = {1, 2, 3, 4};
+//        System.out.println(maximumProduct(nums));
+
+        // 1128
+        int[][] dominoes ={{1, 2}, {2, 1}, {3, 4}, {5, 6}};
+        System.out.println(numEquivDominoPairs(dominoes));
 
 
     }
@@ -649,6 +653,20 @@ public class section1 {
         }
 
         return Math.max(min1 * min2 * max1, max3 * max2 * max1);
+    }
+
+    // 1128. 等价多米诺骨牌对的数量 二元数组，正反相等
+    public static int numEquivDominoPairs(int[][] dominoes) {
+        // 既然是二元组，不妨把两个数拼成一个数做降维操作
+        // 本题的核心思想需要比较和交换，让二元组中第一个数始终小于第二个数，
+        // 然后让第一个数当十位，第二个数当个位，这样组成的一个两位数当作计数数组的下标即可
+        // [a, b] => if (a < b) {a * 10 + b} else {b * 10 + a}
+        int[] copy = new int[100];
+        int answer = 0;
+        for(int[] arr : dominoes) {
+            answer += arr[0] < arr[1] ? copy[arr[0] * 10 + arr[1]]++ : copy[arr[1] * 10 + arr[0]]++;
+        }
+        return answer;
     }
 
 }
