7edc4f03215a3065ff1f7889af9ecbf91635fde2
arthurye <soyellowjack@gmail.com>
2021-02-08 15:14:31 +0800

    add new commit question 633

---
+++ leetcodetest/src/part_1/medium/array/JudgeSquareSum633.java
@@ -0,0 +1,26 @@
+package part_1.medium.array;
+
+public class JudgeSquareSum633 {
+
+    public static void main(String[] args) {
+        System.out.println(judgeSquareSum(3));
+    }
+
+    public static boolean judgeSquareSum(int c) {
+        if(c < 0) {
+            return false;
+        }
+        int low = 0, high = (int) Math.sqrt(c);
+        while(low <= high) {
+            int sum = low * low + high * high;
+            if(sum == c) {
+                return true;
+            }else if(sum < c) {
+                low++;
+            }else {
+                high--;
+            }
+        }
+        return false;
+    }
+}
