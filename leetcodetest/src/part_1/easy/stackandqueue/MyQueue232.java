139a0895276d5bc0679cda9fb3aaa4485b72744e
arthurye <soyellowjack@gmail.com>
2021-05-02 23:35:09 +0800

    add new question 232

---
+++ leetcodetest/src/part_1/easy/stackandqueue/MyQueue232.java
@@ -0,0 +1,50 @@
+package part_1.easy.stackandqueue;
+
+import java.util.Stack;
+
+public class MyQueue232 {
+
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+
+    /*
+     * 解题思路：栈的顺序为后进先出，而队列的顺序为先进先出。使用两个栈实现队列，一个元素需要经过两个栈才能出队            * 列，在经过第一个栈时元素顺序被反转，经过第二个栈时再次被反转，此时就是先进先出顺序。
+     */
+
+    /** Initialize your data structure here. */
+    public MyQueue232() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        in.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        in2out();
+        return out.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        in2out();
+        return out.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+
+    private void in2out() {
+        while(out.isEmpty()) {
+            while(!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+    }
+
+}
