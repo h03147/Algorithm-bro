44345ede6417faf688880c9a792a706ff79daff7
arthurye <soyellowjack@gmail.com>
2021-07-03 21:10:47 +0800

    add new question swordOffer 06 18

---
+++ leetcodetest/src/part_1/easy/linkedlist/deleteNode_swordOffer18.java
@@ -0,0 +1,16 @@
+package part_1.easy.linkedlist;
+
+public class deleteNode_swordOffer18 {
+
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while(cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if(cur != null) pre.next = cur.next;
+        return head;
+    }
+
+}
