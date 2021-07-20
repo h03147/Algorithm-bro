b9cb105e962369274fb670827a12a3d2df0f4ad5
arthurye <soyellowjack@gmail.com>
2021-07-20 23:18:26 +0800

    add new question 235 236

---
+++ leetcodetest/src/part_1/easy/tree/LowestCommonAncestor235.java
@@ -0,0 +1,11 @@
+package part_1.easy.tree;
+
+public class LowestCommonAncestor235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+}
