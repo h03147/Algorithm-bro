05f00186012ac1fae64ee24f2950322788c49812
arthurye <soyellowjack@gmail.com>
2021-08-07 19:35:35 +0800

    add new question swordOffer 48

---
+++ leetcodetest/src/part_1/medium/dp/lengthOfLongestSubstring_swordOffer48.java
@@ -0,0 +1,17 @@
+package part_1.medium.dp;
+
+public class lengthOfLongestSubstring_swordOffer48 {
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = dic.getOrDefault(s.charAt(j), -1); // 获取索引 i
+            dic.put(s.charAt(j), j); // 更新哈希表
+            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        }
+        return res;
+    }
+
+}
