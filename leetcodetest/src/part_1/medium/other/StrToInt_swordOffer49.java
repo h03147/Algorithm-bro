08d78cb5d3290e6634baa43ad21e1b8020084055
arthurye <soyellowjack@gmail.com>
2021-08-25 23:42:36 +0800

    add new question swordOffer 49

---
+++ leetcodetest/src/part_1/medium/other/StrToInt_swordOffer49.java
@@ -0,0 +1,21 @@
+package part_1.medium.other;
+
+public class StrToInt_swordOffer49 {
+
+    public int StrToInt(String str) {
+        if (str == null || str.length() == 0)
+            return 0;
+        boolean isNegative = str.charAt(0) == '-';
+        int ret = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (i == 0 && (c == '+' || c == '-'))  /* 符号判定 */
+                continue;
+            if (c < '0' || c > '9')                /* 非法输入 */
+                return 0;
+            ret = ret * 10 + (c - '0');
+        }
+        return isNegative ? -ret : ret;
+    }
+
+}
