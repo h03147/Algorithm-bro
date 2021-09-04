338dedb070aa57bca962c7b1109393b4f4e249d6
arthurye <soyellowjack@gmail.com>
2021-09-04 21:07:33 +0800

    add new question 2

---
+++ leetcodetest/src/part_1/medium/linkedlist/AddTwoNumbers2.java
@@ -0,0 +1,30 @@
+package part_1.medium.linkedlist;
+
+public class AddTwoNumbers2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            if(l1 != null) {
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if(carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
+}
